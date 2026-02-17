6b0d3a4009a2de10a7776d7adbeefd38e966d7f1
Edgleyson@pm.me <Edgleyson@pm.me>
2026-02-17 00:50:18 +0000

    q1

---
+++ models/intermediate/int_trips_unioned.sql
@@ -0,0 +1,12 @@
+{{ config(materialized='table') }}
+
+with green as (
+    select * from {{ ref('stg_green_tripdata') }}
+),
+yellow as (
+    select * from {{ ref('stg_yellow_tripdata') }}
+)
+
+select * from green
+union all
+select * from yellow
